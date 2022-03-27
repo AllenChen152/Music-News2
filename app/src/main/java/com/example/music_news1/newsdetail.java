553067b58ea93e2f68aa5079753beba6a4730337
AllenChen152 <772697218@qq.com>
2022-03-27 12:49:03 +0800

    嵌入网页测试

---
+++ app/src/main/java/com/example/music_news1/newsdetail.java
@@ -0,0 +1,15 @@
+package com.example.music_news1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class newsdetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsdetail);
+    }
+}
