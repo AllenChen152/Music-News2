853ac7cca6c994b44c43452bd6ba5ead19cef961
AllenChen152 <772697218@qq.com>
2022-03-28 11:33:55 +0800

    跳转

--- app/src/main/java/com/example/music_news1/MainActivity.java
+++ app/src/main/java/com/example/music_news1/MainActivity.java
@@ -10,31 +10,31 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Button button1=(Button) findViewById(R.id.button);
-        //button1.setOnClickListener(new View.OnClickListener(){
-        //    public void onClick(View v){
-        //        Intent intent1 = new Intent(MainActivity.this, newsdetail.class);
-        //        startActivity(intent1);
-        //    }
-        //});
-        WebView webView=findViewById(R.id.webview);
+        Button button1=(Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent1 = new Intent(MainActivity.this, newsdetail.class);
+                startActivity(intent1);
+            }
+        });
+        /*WebView webView=findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://yue.ifeng.com/shanklist/26-35261-/");
+        webView.loadUrl("http://yue.ifeng.com/shanklist/26-35261-/");*/
 
     }
-    public class WebViewClientDemo extends WebViewClient {
+    /*public class WebViewClientDemo extends WebViewClient {
         @Override
         // 在WebView中而不是默认浏览器中显示页面
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
-    }
+    }*/
 
 }
