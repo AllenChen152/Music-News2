342124426cd63c6d42783067c8d60047de95a82e
AllenChen152 <772697218@qq.com>
2022-03-31 15:39:17 +0800

    几个页面修改后的

--- app/src/main/java/com/example/music_news1/PersonActivity.java
+++ app/src/main/java/com/example/music_news1/PersonActivity.java
@@ -3,11 +3,15 @@ package com.example.music_news1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PersonActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ImageButton imageButton5,imageButton6,imageButton7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,22 @@ public class PersonActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("爱乐——个人中心");
+        imageButton5=(ImageButton) findViewById(R.id.imageButton5);
+        imageButton6=(ImageButton) findViewById(R.id.imageButton6);
+        imageButton7=(ImageButton) findViewById(R.id.imageButton7);
+
+        imageButton5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent1 = new Intent(PersonActivity.this, MainActivity2.class);
+                startActivity(intent1);
+            }
+        });
+        imageButton6.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent1 = new Intent(PersonActivity.this, quanzi.class);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/music_news1/quanzi.java
+++ app/src/main/java/com/example/music_news1/quanzi.java
@@ -1,19 +1,39 @@
 package com.example.music_news1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.widget.Toolbar;
 
 public class quanzi extends Activity {
 
     private Toolbar toolbar;
+    private ImageButton imageButton5,imageButton6,imageButton7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.one);
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("爱乐——圈子");
+        imageButton5=(ImageButton) findViewById(R.id.imageButton5);
+        imageButton6=(ImageButton) findViewById(R.id.imageButton6);
+        imageButton7=(ImageButton) findViewById(R.id.imageButton7);
+
+        imageButton5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent1 = new Intent(quanzi.this, MainActivity2.class);
+                startActivity(intent1);
+            }
+        });
+        imageButton7.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent1 = new Intent(quanzi.this, PersonActivity.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 
